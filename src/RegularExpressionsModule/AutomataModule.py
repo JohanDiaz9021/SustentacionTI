316021bf12f0dce1af8ee70f992a30be1154716c
Johan Diaz <joansebas9021@hotmail.com>
2024-04-16 20:56:15 -0500

    automata

--- src/RegularExpressionsModule/AutomataModule.py
+++ src/RegularExpressionsModule/AutomataModule.py
@@ -20,13 +20,13 @@ def create_telefono_dfa():
         telefono_dfa.add_transition("q5", str(i), "q6")
         telefono_dfa.add_transition("q6", str(i), "q7")
         telefono_dfa.add_transition("q7", str(i), "q8")
-    telefono_dfa.add_transition("q8", "-", "q9")  # Considerando el guión en un número de teléfono
+    telefono_dfa.add_transition("q8", "-", "q9")
     return telefono_dfa
 
 def create_identificacion_dfa():
     identificacion_dfa = DeterministicFiniteAutomaton()
     identificacion_dfa.add_start_state("q0")
-    identificacion_dfa.add_final_state("q6")
+    identificacion_dfa.add_final_state("q10")
     for i in range(10):  # Dígitos del 0 al 9
         identificacion_dfa.add_transition("q0", str(i), "q1")
         identificacion_dfa.add_transition("q1", str(i), "q2")
@@ -34,11 +34,8 @@ def create_identificacion_dfa():
         identificacion_dfa.add_transition("q3", str(i), "q4")
         identificacion_dfa.add_transition("q4", str(i), "q5")
         identificacion_dfa.add_transition("q5", str(i), "q6")
-    for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":  # Letras mayúsculas
-        identificacion_dfa.add_transition("q0", letter, "q1")
-        identificacion_dfa.add_transition("q1", letter, "q2")
-        identificacion_dfa.add_transition("q2", letter, "q3")
-        identificacion_dfa.add_transition("q3", letter, "q4")
-        identificacion_dfa.add_transition("q4", letter, "q5")
-        identificacion_dfa.add_transition("q5", letter, "q6")
+        identificacion_dfa.add_transition("q6", str(i), "q7")
+        identificacion_dfa.add_transition("q7", str(i), "q8")
+        identificacion_dfa.add_transition("q8", str(i), "q9")
+        identificacion_dfa.add_transition("q9", str(i), "q10")  
     return identificacion_dfa
